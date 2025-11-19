2af820d1f837ce867615f74e77c600d6142c4c84
jayanandyk <50801142+jayanandyk@users.noreply.github.com>
2025-11-18 21:59:36 -0800

    Scss file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { finalize } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   availableTasks: AvailableTask[] = [];
